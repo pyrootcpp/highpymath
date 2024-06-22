87082758f57560ffc252c26c724bb8edce9a26e9
pyrootcpp <pyrootcpp@outlook.de>
2024-06-22 13:39:56 +0200

    Update

--- highpymath/__init__.py
+++ highpymath/__init__.py
@@ -2,11 +2,35 @@ from .highpymath import sum as _sum
 from .highpymath import sub as _sub
 from .highpymath import mul as _mul
 from .highpymath import div as _div
+from .highpymath import MathValueError as _mve
 
-__all__ = ['sum', 'sub', 'mul', 'div']
+__all__ = ['sum', 'sub', 'mul', 'div', 'MathValueError']
 
-def sum(a: float, b: float):
+class MathValueError(_mve):
     """
-    Create the Summary from 2 Numbers.
+    Exception Class for Math Value Errors.
     """
-    return _sum(a=a, b=b)
+    def __init__(self, *args: object):
+        """
+        Initial the Exception Class with Given Arguments.
+        """
+        self.args_list = list(args)
+        self.args_str = str(args)
+        super().__init__(*args)
+
+def sum(a: any, b: any, return_int: bool = False):
+    """
+    Create the Summary of 2 Numbers.
+    """
+    if not isinstance(a, (int, float)):
+        raise MathValueError("a must be a number")
+    if not isinstance(b, (int, float)):
+        raise MathValueError("b must be a number")
+    if isinstance(a, int):
+        a = float(a)
+    if isinstance(b, int):
+        b = float(b)
+    if return_int:
+        return int(_sum(a=a, b=b))
+    else:
+        return _sum(a=a, b=b)
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,40 @@
 use pyo3::prelude::*;
+use pyo3::exceptions::PyException;
+use pyo3::create_exception;
 
-/// Formats the sum of two numbers as string.
+create_exception!(highpymath, MathValueError, PyException);
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn sum(a: f32, b: f32) -> PyResult<f32> {
+    Ok(a + b)
+}
+
+#[pyfunction]
+fn sub(a: f32, b: f32) -> PyResult<f32> {
+    Ok(a - b)
+}
+
+#[pyfunction]
+fn mul(a: f32, b: f32) -> PyResult<f32> {
+    Ok(a * b)
+}
+
+#[pyfunction]
+fn div(a: f32, b: f32) -> PyResult<f32> {
+    if b == 0.0 {
+        Err(MathValueError::new_err("Division by Zero"))
+    } else {
+        Ok(a / b)
+    }
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn highpymath(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn highpymath(m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum, m)?)?;
+    m.add_function(wrap_pyfunction!(sub, m)?)?;
+    m.add_function(wrap_pyfunction!(mul, m)?)?;
+    m.add_function(wrap_pyfunction!(div, m)?)?;
+    m.add("MathValueError", m.py().get_type::<MathValueError>())?;
     Ok(())
 }
